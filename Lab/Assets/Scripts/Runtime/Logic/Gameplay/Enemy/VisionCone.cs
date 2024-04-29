547d66559b11789d45ff4445adf6602be5ecb4f1
Gleb <116743505+GPLoreGames@users.noreply.github.com>
2024-04-29 23:21:40 +0300

    Added enemy vision cone

---
+++ Lab/Assets/Scripts/Runtime/Logic/Gameplay/Enemy/VisionCone.cs
@@ -0,0 +1,84 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Runtime.Logic.Gameplay.Enemy
+{
+    // Original from https://www.youtube.com/watch?v=luLrhoTZYD8
+    public class VisionCone : MonoBehaviour
+    {
+        public MeshFilter m_ConeMeshFilter;
+        public float m_VisionRange;
+        public float m_VisionAngle;
+        // Layer with objects that obstruct the enemy view, like walls, for example.
+        public LayerMask m_VisionObstructingLayer;
+        // The vision cone will be made up of triangles, the higher this value is the prettier the vision cone will be.
+        public int m_VisionConeResolution = 120;
+
+        private float RadianAngle => m_VisionAngle * Mathf.Deg2Rad;
+        private Mesh _visionConeMesh;
+        
+        //Create all of these variables, most of them are self explanatory, but for the ones that aren't i've added a comment to clue you in on what they do
+        //for the ones that you dont understand dont worry, just follow along.
+
+        private void Start() => _visionConeMesh = new();
+
+        public void Update()
+        {
+            // Calling the vision cone function every frame just so the cone is updated every frame.
+            DrawVisionCone(); 
+        }
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Start();
+            DrawVisionCone();
+        }
+#endif
+
+        // This method creates the vision cone mesh.
+        private void DrawVisionCone()
+        {
+            var triangles = new int[(m_VisionConeResolution - 1) * 3];
+            var vertices = new Vector3[m_VisionConeResolution + 1];
+            vertices[0] = Vector3.zero;
+            
+            float currentAngle = -RadianAngle / 2;
+            float angleIncrement = RadianAngle / (m_VisionConeResolution - 1);
+
+            for (int i = 0; i < m_VisionConeResolution; i++)
+            {
+                var sine = Mathf.Sin(currentAngle);
+                var cosine = Mathf.Cos(currentAngle);
+                
+                Vector3 raycastDirection = transform.forward * cosine + transform.right * sine;
+                Vector3 vertForward = Vector3.forward * cosine + Vector3.right * sine;
+                
+                if (Physics.Raycast(transform.position, raycastDirection, out RaycastHit hit, m_VisionRange, m_VisionObstructingLayer))
+                {
+                    vertices[i + 1] = vertForward * hit.distance;
+                }
+                else
+                {
+                    vertices[i + 1] = vertForward * m_VisionRange;
+                }
+
+
+                currentAngle += angleIncrement;
+            }
+            
+            for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
+            {
+                triangles[i] = 0;
+                triangles[i + 1] = j + 1;
+                triangles[i + 2] = j + 2;
+            }
+            
+            _visionConeMesh.Clear();
+            _visionConeMesh.vertices = vertices;
+            _visionConeMesh.triangles = triangles;
+            m_ConeMeshFilter.mesh = _visionConeMesh;
+        }
+    }
+}
--- Lab/Assets/Scripts/Runtime/Logic/Gameplay/Spawning/EnemySpawnMarker.cs
+++ Lab/Assets/Scripts/Runtime/Logic/Gameplay/Spawning/EnemySpawnMarker.cs
@@ -1,4 +1,5 @@
 ﻿using Runtime.Configs;
+using Runtime.Logic.Gameplay.Enemy;
 using UnityEngine;
 
 namespace Runtime.Logic.Gameplay.Spawning
@@ -6,11 +7,21 @@ namespace Runtime.Logic.Gameplay.Spawning
     public class EnemySpawnMarker : SpawnMarker
     {
         [SerializeField] private EnemyConfig m_EnemyConfig;
+        [SerializeField] private VisionCone m_VisionCone;
 
         public EnemyConfig Config => m_EnemyConfig;
         
 #if UNITY_EDITOR
-        private void OnValidate() => m_MarkerType = SpawnMarkerType.Enemy;
+        private void OnValidate()
+        {
+            m_MarkerType = SpawnMarkerType.Enemy;
+            
+            if (m_VisionCone != null)
+            {
+                m_VisionCone.m_VisionRange = m_EnemyConfig.m_ViewDistance;
+                m_VisionCone.m_VisionAngle = m_EnemyConfig.m_ViewAngle;
+            }
+        }
 
         protected override void OnDrawGizmos()
         {
